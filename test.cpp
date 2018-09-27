ffb158744b7194c233c828300bfbbc58a99579f9
Milo Knowles <mknowles@mit.edu>
2018-09-27 14:28:49 -0400

    Add constructor test

--- test.cpp
+++ test.cpp
@@ -1,8 +1,17 @@
 #include <gtest/gtest.h>
 
+#include "routing_problem.hpp"
+
+using namespace routing;
+
+/**
+ * @brief Make sure that a problem is loaded in properly.
+ */
 TEST(RoutingProblemTest, TestConstructor)
 {
-	EXPECT_TRUE(true);
+	const std::string problem_path("../sample/problem.txt");
+	std::cout << "Loading from: " << problem_path << std::endl;
+	RoutingProblem problem(problem_path);
 }
 
 // Run all the tests that were declared with TEST().
